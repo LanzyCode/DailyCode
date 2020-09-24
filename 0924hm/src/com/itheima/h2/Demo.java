6de607325222338fc4d9d5281aefd149e2630222
Lanzy <8689339@qq.com>
2020-09-24 19:40:04 +0800

    0924Update003

---
+++ 0924hm/src/com/itheima/h2/Demo.java
@@ -0,0 +1,18 @@
+package com.itheima.h2;
+
+import java.math.BigDecimal;
+
+public class Demo {
+    public static void main(String[] args) {
+        Student stu1 = new Student("张三", "男", 20, 79.5);
+        Student stu2 = new Student("李四", "女", 21, 80.2);
+        Student stu3 = new Student("王五", "男", 22, 77.9);
+        Student stu4 = new Student("周六", "男", 20, 55.8);
+        Student stu5 = new Student("赵七", "女", 21, 99.9);
+
+        double sum = stu1.getScore() + stu2.getScore() + stu3.getScore() + stu4.getScore() + stu5.getScore();
+        double avg = sum / 5;
+        BigDecimal bd = new BigDecimal(avg);
+        System.out.println(bd.divide(BigDecimal.valueOf(1), 2, BigDecimal.ROUND_HALF_UP));
+    }
+}
---
+++ 0924hm/src/com/itheima/h3/Demo.java
@@ -0,0 +1,23 @@
+package com.itheima.h3;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr = {1, 332, 43, 25, 3324, 34, 335, 4, 45, 2, 43, 52, 54};
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
